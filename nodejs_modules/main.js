ac07581d73983d12c1f25a4af6eb013dc4952a1e
doriguzman <dorianguzman@ac.c4q.nyc>
2017-11-14 19:36:30 -0500

    new

---
+++ nodejs_modules/main.js
@@ -0,0 +1,12 @@
+const mathModule=require('./math-module.js')
+
+let findSum = mathModule.sum(2,3);
+let findMultiply= mathModule.multiply(2,3);
+let findDivide=mathModule.divide(4,2);
+let findSquare=mathModule.square(2,2);
+
+
+console.log(findSum)
+console.log(findDivide)
+console.log(findMultiply)
+console.log(findSquare)
---
+++ nodejs_modules/math-module.js
@@ -0,0 +1,16 @@
+let sum= (num1,num2)=> num1+num2
+
+let multiply=(num1,num2)=> num1*num2
+
+let divide=(num1, num2)=> num1/num2
+
+let square = (num1)=> Math.pow(num1,2)
+
+
+module.exports = {
+    sum: sum,
+    multiply: multiply,
+    divide: divide,
+    square: square
+  }
+
